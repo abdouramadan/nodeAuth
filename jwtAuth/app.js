87417a780ac36c8d0c8546cf45375fc6aa563068
abdouramadan <abdouramadan36@gmail.com>
2020-11-19 11:03:50 +0300

    added new ...

--- jwtAuth/app.js
+++ jwtAuth/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const port     = 3000
 const router = require('./route/authRoutes')
 const bodyParser    = require('body-parser')
+const cokkieParser = require('cookie-parser')
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cokkieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -24,6 +26,25 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(router)
+
+/*
+    // set cookies
+    app.get('/set-cookies', (req, res) => {
+        // res.setHeader('Set-Cookie', 'newUser=true')
+
+        // @or
+        res.cookie('name', 'abdou ramadan')
+        res.cookie('newUser', false, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
+        res.send('you get the cookie ...')
+    })
+
+    // read cookies
+    app.get('/read-cookies', (req, res) => {
+        const cookies = req.cookies
+        console.log(cookies)
+        res.json(cookies)
+    })
+*/
 
 // server
 app.listen(port, () => console.log('server running'))
